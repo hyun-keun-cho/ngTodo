fe15365c569fcfdf8616271ead7f28e3c1e2260a
renee13wi <renee13wi@gmail.com>
2018-04-14 18:22:55 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,49 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { AngularComponent } from './angular/angular.component';
+import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {UserService} from './user.service';
+import {HttpClientModule} from '@angular/common/http';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full'},
+  {
+    path: 'index', component: IndexComponent, children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'jquery', component: JqueryComponent },
+      { path: 'angular', component: AngularComponent }
+    ]
+  }
+  // { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule' } // lazyloading
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     HomeComponent,
-    JqueryComponent
+    JqueryComponent,
+    AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
